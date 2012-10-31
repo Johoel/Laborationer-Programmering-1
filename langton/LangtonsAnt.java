a07fa497b4f123de04cc346297c2681c67f4f933
Joel Abrahamsson <joel.abrahamsson1@gmail.com>
2012-10-31 21:01:13 +0100

    Add getPlane-method. Returns the plane which the ant moves on
    Add getAnt-method. Returns the position of the ant
    
    Remove main-method

--- langton/LangtonsAnt.java
+++ langton/LangtonsAnt.java
@@ -15,33 +15,39 @@ public class LangtonsAnt
 
   private Point ant;                      // The ant's position
   private int direction = 0;              // The ant's direction
-  private HashMap<Point, Boolean> field;  // The field which the ant moves on
+  private HashMap<Point, Boolean> plane;  // The plane which the ant moves on
 
   public LangtonsAnt()
   {
     // Create a new Map to hold coordinates for ant
-    field = new HashMap<Point, Boolean>();
+    plane = new HashMap<Point, Boolean>();
+  }
+  
+  public Point getAnt()
+  {
+    return ant;
+  }
+
+  public HashMap<Point, Boolean> getPlane()
+  {
+    return plane;
   }
 
   public void step()
   {
     // If the ant is on a black square
-    if (field.get(ant))
+    if (plane.get(ant))
     {
       direction = (direction - 1) % 4;
-      field.put(ant, false);
+      plane.put(ant, false);
     }
     else  // If the ant is on a white spot
     {
       direction = (direction + 1) % 4;
-      field.put(ant, true);
+      plane.put(ant, true);
     }
 
     ant.translate(directions[direction].x,
                   directions[direction].y);
-  }
-
-  public static void main(String[] args)
-  {
   }
 }
