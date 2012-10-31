1c18e9e17f6e75ff5c3f67d64228981c4a0d804d
Joel Abrahamsson <joel.abrahamsson1@gmail.com>
2012-10-31 22:42:40 +0100

    Use HashSet instead of HashMap for plane
    Implement simple GUI

--- langton/LangtonsAnt.java
+++ langton/LangtonsAnt.java
@@ -1,26 +1,27 @@
 package langton;
 
 import java.awt.Point;
-import java.util.HashMap;
+import java.util.HashSet;
 
 public class LangtonsAnt
 {
   private final Point[] directions =
   {
-    new Point(1, 0),
     new Point(0, 1),
-    new Point(-1, 0),
-    new Point(0, -1)
+    new Point(1, 0),
+    new Point(0, -1),
+    new Point(-1, 0)
   };
 
   private Point ant;                      // The ant's position
   private int direction = 0;              // The ant's direction
-  private HashMap<Point, Boolean> plane;  // The plane which the ant moves on
+  private HashSet<Point> plane;           // The plane which the ant moves on
 
-  public LangtonsAnt()
+  public LangtonsAnt(int width, int height)
   {
     // Create a new Map to hold coordinates for ant
-    plane = new HashMap<Point, Boolean>();
+    plane = new HashSet<Point>();
+    ant = new Point(width / 2, height / 2);
   }
   
   public Point getAnt()
@@ -28,7 +29,7 @@ public class LangtonsAnt
     return ant;
   }
 
-  public HashMap<Point, Boolean> getPlane()
+  public HashSet<Point> getPlane()
   {
     return plane;
   }
@@ -36,18 +37,24 @@ public class LangtonsAnt
   public void step()
   {
     // If the ant is on a black square
-    if (plane.get(ant))
+    if (plane.contains(ant))
     {
-      direction = (direction - 1) % 4;
-      plane.put(ant, false);
+      direction = (4 + (direction - 1)) % 4;
+      plane.remove(ant);
     }
     else  // If the ant is on a white spot
     {
       direction = (direction + 1) % 4;
-      plane.put(ant, true);
+      plane.add((Point)ant.clone());
     }
 
     ant.translate(directions[direction].x,
                   directions[direction].y);
+
+    //System.out.printf("Ant (%d, %d), direction[%d] = (%s)\n",
+                      //ant.x,
+                      //ant.y,
+                      //direction,
+                      //directions[direction]);
   }
 }
--- langton/gui/LangtonGUI.java
+++ langton/gui/LangtonGUI.java
@@ -2,12 +2,26 @@ package langton.gui;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import java.awt.image.BufferedImage;
-import javax.swing.*;
+import java.util.HashSet;
+import java.util.Iterator;
+import javax.swing.JFrame;
+import javax.swing.Timer;
 
-public class LangtonGUI extends JFrame
+import langton.LangtonsAnt;
+
+public class LangtonGUI extends JFrame implements ActionListener
 {
+  private final int BLACK = Color.BLACK.getRGB();
+  private final int RED = Color.RED.getRGB();
+
+  private LangtonsAnt langton;
   private BufferedImage planeImage;
+  private Timer timer;
+  private long steps = 0;
 
   public LangtonGUI()
   {
@@ -17,7 +31,22 @@ public class LangtonGUI extends JFrame
     setSize(800, 600);
     setResizable(false);
 
+    langton = new LangtonsAnt(800, 600);
+
     planeImage = createPlane(800, 600);
+
+    timer = new Timer(10, this);
+    timer.start();
+  }
+
+  private void clearImage(BufferedImage image)
+  {
+    Graphics g = image.createGraphics();
+
+      g.setColor(Color.WHITE);
+      g.fillRect(0, 0, image.getWidth(), image.getHeight());
+
+      g.dispose();
   }
 
   private BufferedImage createPlane(int width, int height)
@@ -27,14 +56,40 @@ public class LangtonGUI extends JFrame
 
     BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 
-    Graphics g = image.createGraphics();
-      
-      g.setColor(Color.WHITE);
-      g.fillRect(0, 0, width, height);
-
-      g.dispose();
+    clearImage(image);
 
     return image;
+  }
+
+  public void actionPerformed(ActionEvent e)
+  {
+    HashSet<Point> plane = langton.getPlane();
+    Point ant = langton.getAnt();
+
+    clearImage(planeImage);
+
+    Iterator<Point> planeIterator = plane.iterator();
+
+    while (planeIterator.hasNext())
+    {
+      Point currentPoint = planeIterator.next();
+      
+      if (currentPoint.x > 0 && currentPoint.x < 800 &&
+          currentPoint.y > 0 && currentPoint.y < 600)
+      {
+        planeImage.setRGB(currentPoint.x, currentPoint.y, BLACK);
+      }
+    }
+
+    planeImage.setRGB(ant.x, ant.y, RED);
+
+    steps++;
+    setTitle("Langton's Ant (" + steps + ")");
+
+    if (steps % 100 == 0)
+      repaint();
+
+    langton.step();
   }
 
   public void paint(Graphics g)
