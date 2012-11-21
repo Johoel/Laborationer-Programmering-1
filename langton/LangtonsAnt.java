19c77abe0a0eb31b2abff807ad76b05da5d2f847
Joel Abrahamsson <joel.abrahamsson1@gmail.com>
2012-11-21 22:15:35 +0100

    Add options to constructor for creating noise on the plane and put the ant on a random cell at start
    Change the direction-array
    
    Add and improve comments

--- langton/LangtonsAnt.java
+++ langton/LangtonsAnt.java
@@ -2,38 +2,103 @@ package langton;
 
 import java.awt.Point;
 import java.util.HashSet;
+import java.util.Random;
 
 public class LangtonsAnt
 {
+  /*
+   *  The possible directions the ant can move in.
+   */
   private final Point[] directions =
   {
-    new Point(0, 1),
-    new Point(1, 0),
+    new Point(-1, 0),
     new Point(0, -1),
-    new Point(-1, 0)
+    new Point(1, 0),
+    new Point(0, 1)
   };
 
   private Point ant;                      // The ant's position
   private int direction = 0;              // The ant's direction
   private HashSet<Point> plane;           // The plane which the ant moves on
 
+  /**
+   *  Creates an empty plane and puts the ant in the middle.
+   *
+   *  @param width  the width of the plane
+   *  @param height the height of the plane
+   */
   public LangtonsAnt(int width, int height)
   {
-    // Create a new Map to hold coordinates for ant
     plane = new HashSet<Point>();
     ant = new Point(width / 2, height / 2);
   }
+
+  /**
+   *  Creates a plane. If randomizePlane is true, cells will be
+   *  set at random to either black or white. If randomStart is true, the ant's starting
+   *  position will be random.
+   *
+   *  @param width          the width of the plane
+   *  @param height         the height of the plane
+   *  @param randomizePlane if true, randomly set the color of all cells on the plane
+   *  @param randomStart    if true, the ant will be put on a random cell on the plane
+   */
+  public LangtonsAnt(int width, int height, boolean randomizeBoard, boolean randomStart)
+  {
+    this(width, height);
+
+    if (randomizeBoard)
+      randomizeBoard(width, height);
+
+    if (randomStart)
+      ant = new Point((int)(Math.random() * width),
+                      (int)(Math.random() * height));
+  }
+
+  /**
+   *  Sets a random number of cells to black to create noise on the plane.
+   *
+   *  @param width  the widht of the plane
+   *  @param height the height of the plane
+   */
+  private void randomizeBoard(int width, int height)
+  {
+    Random random = new Random(System.nanoTime());
+
+    int numberOfCells = random.nextInt((width * height) / 10);
+    for (int i = 0; i < numberOfCells; i++)
+    {
+      plane.add(new Point(random.nextInt(width), random.nextInt(height)));
+
+      random.setSeed(System.nanoTime());
+    }
+  }
   
+  /**
+   *  Returns the position of the ant.
+   *
+   *  @return the position of the ant
+   *  @see    Point
+   */
   public Point getAnt()
   {
     return ant;
   }
 
+  /**
+   *  Returns the plane.
+   *
+   *  @return the plane
+   *  @see    HashMap
+   */
   public HashSet<Point> getPlane()
   {
     return plane;
   }
 
+  /**
+   *  Make the ant move
+   */
   public void step()
   {
     // If the ant is on a black square
@@ -50,11 +115,5 @@ public class LangtonsAnt
 
     ant.translate(directions[direction].x,
                   directions[direction].y);
-
-    //System.out.printf("Ant (%d, %d), direction[%d] = (%s)\n",
-                      //ant.x,
-                      //ant.y,
-                      //direction,
-                      //directions[direction]);
   }
 }
--- langton/gui/LangtonGUI.java
+++ langton/gui/LangtonGUI.java
@@ -19,26 +19,49 @@ public class LangtonGUI extends JFrame implements ActionListener
   private final int RED = Color.RED.getRGB();
 
   private LangtonsAnt langton;
+  private int width;
+  private int height;
   private BufferedImage planeImage;
   private Timer timer;
   private long steps = 0;
 
-  public LangtonGUI()
-  {
-    super("Lagton's Ant");
 
+  /**
+   *  Creates a window of given width and height. The width and height
+   *  is used for creating the plane as well.
+   *
+   *  @param width          window width
+   *  @param height         window height
+   *  @param randomizePlane set random cells on the plane to black
+   *  @param randomStart    put the ant on a random cell
+   *  @see                  langton.LangtonsAnt;
+   */
+  public LangtonGUI(int width, int height, boolean randomizePlane, boolean randomStart)
+  {
+    super("Langton's Ant");
+
+    this.width = width;
+    this.height = height;
+
+    // Change window settings
     setDefaultCloseOperation(EXIT_ON_CLOSE);
-    setSize(800, 600);
+    setSize(width, height);
     setResizable(false);
 
-    langton = new LangtonsAnt(800, 600);
+    langton = new LangtonsAnt(width, height, randomizePlane, randomStart);
 
-    planeImage = createPlane(800, 600);
+    planeImage = createPlane(width, height);
 
     timer = new Timer(10, this);
     timer.start();
   }
 
+  /**
+   *  Clears the given image. Draws a solid white rectangle over the whole image.
+   *
+   *  @param image image to clear
+   *  @see         BufferedImage
+   */
   private void clearImage(BufferedImage image)
   {
     Graphics g = image.createGraphics();
@@ -49,6 +72,13 @@ public class LangtonGUI extends JFrame implements ActionListener
       g.dispose();
   }
 
+  /**
+   *  Creates a new image of given width and height and clears it.
+   *
+   *  @param width  image width
+   *  @param hegiht image height
+   *  @see          BufferedImage
+   */
   private BufferedImage createPlane(int width, int height)
   {
     if (width <= 0 && height <= 0)
@@ -61,6 +91,9 @@ public class LangtonGUI extends JFrame implements ActionListener
     return image;
   }
 
+  /**
+   *  Move the ant and update the image.
+   */
   public void actionPerformed(ActionEvent e)
   {
     HashSet<Point> plane = langton.getPlane();
@@ -74,14 +107,18 @@ public class LangtonGUI extends JFrame implements ActionListener
     {
       Point currentPoint = planeIterator.next();
       
-      if (currentPoint.x > 0 && currentPoint.x < 800 &&
-          currentPoint.y > 0 && currentPoint.y < 600)
+      if (currentPoint.x > 0 && currentPoint.x < width &&
+          currentPoint.y > 0 && currentPoint.y < height)
       {
         planeImage.setRGB(currentPoint.x, currentPoint.y, BLACK);
       }
     }
 
-    planeImage.setRGB(ant.x, ant.y, RED);
+    if (ant.x > 0 && ant.x < width &&
+        ant.y > 0 && ant.y < height)
+    {
+      planeImage.setRGB(ant.x, ant.y, RED);
+    }
 
     steps++;
     setTitle("Langton's Ant (" + steps + ")");
@@ -92,6 +129,12 @@ public class LangtonGUI extends JFrame implements ActionListener
     langton.step();
   }
 
+  /**
+   *  Draw the image in the window.
+   *
+   *  @see Graphics
+   */
+  @Override
   public void paint(Graphics g)
   {
     super.paint(g);
@@ -100,9 +143,46 @@ public class LangtonGUI extends JFrame implements ActionListener
       g.drawImage(planeImage, 0, 0, null);
   }
 
+  /**
+   *  Takes 4 arguments, all optinal.
+   *
+   *  Usage:
+   *  java langton.gui.LangtonGUI [WIDTH] [HEIGHT] [RANDOMIZE_PLANE] [RANDOM_START]
+   *
+   *    [WIDTH]           - The width of the plane/window
+   *    [HEIGHT]          - The height of the plane/window
+   *    [RANDOMIZE_PLANE] - Create noise on plane
+   *    [RANDOM_START]    - Put the ant on a random cell
+   */
   public static void main(String[] args)
   {
-    LangtonGUI gui = new LangtonGUI();
+    int width = 800,
+        height = 600;
+
+    boolean randomizePlane = false,
+            randomStart = false;
+
+    try
+    {
+      if (args.length > 1)
+      {
+        width = Integer.parseInt(args[0]);
+        height = Integer.parseInt(args[1]);
+      }
+
+      if (args.length > 3)
+      {
+        randomizePlane = Integer.parseInt(args[2]) > 0 ? true : false;
+        randomStart = Integer.parseInt(args[3]) > 0 ? true : false;
+      }
+    }
+    catch (NumberFormatException e)
+    {
+      System.out.println(e.getMessage());
+      e.printStackTrace();
+    }
+
+    LangtonGUI gui = new LangtonGUI(width, height, randomizePlane, randomStart);
     gui.setVisible(true);
   }
 }
