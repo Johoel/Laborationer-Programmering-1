5c472a884c18bf893073b60a485b3422429ed55b
Joel Abrahamsson <joel.abrahamsson1@gmail.com>
2012-11-29 18:39:28 +0100

    Create interface for sorting algorithms

---
+++ sortingalgorithms/SortingAlgorithm.java
@@ -0,0 +1,13 @@
+package sortingalgorithms;
+
+/**
+ *  An interface for sorting algorithms. A class must implement 
+ *  SortingAlgorithm to work with SortingTest.
+ *
+ *  @author  Joel Abrahamsson
+ *  @version %G%
+ */
+public interface SortingAlgorithm
+{
+  public void sort(int[] array);
+}
